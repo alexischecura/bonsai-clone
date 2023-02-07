78807f73e158047b28017d4d05b170d074a47ccb
Alexis Checura <alexischecura@gmail.com>
2023-02-07 20:25:43 -0300

    finish desktop webpage

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-const switchInput = document.querySelector(".switch");
-const price = document.querySelectorAll(".price");
+// Functionality Switch
 
-switchInput.addEventListener("change", () => {
-  price.forEach((plan) => {
-    plan.classList.toggle("show");
+const switchInput = document.querySelector('.switch');
+const price = document.querySelectorAll('.price');
+
+switchInput.addEventListener('change', () => {
+  price.forEach(plan => {
+    plan.classList.toggle('show');
+  });
+});
+
+// Functionality accordion faq
+
+const faqQuestion = document.querySelectorAll('.faq-question');
+const faqContent = document.querySelectorAll('.faq-content');
+
+faqQuestion.forEach((faq, index) => {
+  faq.addEventListener('click', () => {
+    faqContent[index].classList.toggle('hidden')
   });
 });
